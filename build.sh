124a35acbb1ad35e73aafe939d453c6dd629c195
TYDQSoft <166505188+TYDQSoft@users.noreply.github.com>
2024-04-29 17:20:41 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-  	#fpc -XPx86_64-w64-mingw32- -Aas -n -O4 -Si -Sc -Sg -Xd -CX -XXs -Px86_64 -Rintel -Twin64 -Cg -Us system.pas
+ 	#fpc -XPx86_64-w64-mingw32- -Aas -n -O4 -Si -Sc -Sg -Xd -CX -XXs -Px86_64 -Rintel -Twin64 -Cg -Us system.pas
 	fpc -XPx86_64-w64-mingw32- -Aas -n -O4 -Si -Sc -Sg -Xd -CX -XXs -Ct -Px86_64 -Rintel -Twin64 -Cg uefimain.pas
-	x86_64-w64-mingw32-ld --gc-sections -shared -Bsymbolic -Wl,--stack,1073741824 -nostdlib -oformat pei-x86-64 uefimain.o uefi.o system.o -e efi_main -o main.dll
+	x86_64-w64-mingw32-ld --gc-sections -shared -Bsymbolic -nostdlib -oformat=pei-x86-64 uefimain.o uefi.o system.o -e efi_main -o main.dll
 	objcopy -I pei-x86-64 -O efi-app-x86_64 main.dll bootx64.efi
 	dd if=/dev/zero of=fat.img bs=512 count=131072
 	/usr/sbin/mkfs.vfat -F 32 fat.img
